92fbf7697acf981694b2557b6730d0fddafbc74d
loneverse <jobungash@gmail.com>
2022-09-05 10:58:37 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,55 +1,6 @@
 import React, {useState} from 'react';
-import './App.css';
-
 import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView } from 'react-native';
 import Task from './components/Task';
-
-
-import { useAuthState } from 'react-firebase-hooks/auth';
-
-firebase.initializeApp({
-  // config details
-})
-
-function App(){
-  const [user] = useAuthState(auth);
-
-  return (
-    <div className="App">
-      <header>
-        <h1>⚛️🔥💬</h1>
-        <SignOut />
-      </header>
-
-      <section>
-        {user ? <ChatRoom /> : <SignIn />}
-      </section>
-
-    </div>
-  );
-}
-
-function SignIn() {
-
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
-
-  return (
-    <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
-    </>
-  )
-
-}
-
-function SignOut() {
-  return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
-  )
-}
 
 export default function App() {
   const [task, setTask] = useState();
